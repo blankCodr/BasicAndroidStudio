d31794ba22558a25ab57cec8fc558e9f51e1fa54
android-user <android-user@fv.amacclab.edu.ph>
2018-04-07 15:53:42 +0800

    image_exercise

---
+++ app/src/main/java/giancarlo/giancarlo/Image_Activity.java
@@ -0,0 +1,16 @@
+package giancarlo.giancarlo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import giancarlo.giancarlo.R;
+
+public class Image_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
